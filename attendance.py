9ea1901f347a8a8087b7f303b2270e46cb5f15b5
Aswin M Prabhu <aswinmprabhu@gmail.com>
2021-06-21 12:39:15 +0530

    Add config based customization

--- attendance.py
+++ attendance.py
@@ -7,76 +7,85 @@ import argparse
 import json
 
 
-def parse_users(usersFile):
-    users = []
-    with open(usersFile) as f:
-        users = json.load(f)
-    return users
+def parse_config(config_file):
+    config = {}
+    with open(config_file) as f:
+        config = json.load(f)
+    if "interval" not in config:
+        config["interval"] = 20
+    if "driver" not in config:
+        print('"driver" field not found in config')
+        sys.exit(1)
+    if "users" not in config:
+        print('"users" field not found in config')
+        sys.exit(1)
+
+    return config
 
 
-def start_automation(users):
-    driver = webdriver.Chrome(
-        "C:/Users/aswin/auto-attendance/chromedriver.exe")
+def start_automation(users, driver):
+    driver = webdriver.Chrome(driver)
 
     while 1:
         for user in users:
             driver.get("http://moodle.mec.ac.in")
             username_ip = driver.find_element_by_name("username")
             username_ip.clear()
-            username_ip.send_keys(user['email'])
+            username_ip.send_keys(user["email"])
             pass_ip = driver.find_element_by_name("password")
             pass_ip.clear()
-            pass_ip.send_keys(user['password'])
+            pass_ip.send_keys(user["password"])
             pass_ip.send_keys(Keys.RETURN)
 
             if not driver.current_url == "http://moodle.mec.ac.in/my/":
                 print("Failed to log in")
                 sys.exit(1)
 
-            for course in user['courses']:
+            for course in user["courses"]:
                 driver.get(course)
                 course_name_tag = driver.find_elements_by_xpath("//h1")[0]
                 course_name = course_name_tag.text
                 submit_attendance_btns = driver.find_elements_by_xpath(
-                    "//*[contains(text(), 'Submit attendance')]")
+                    "//*[contains(text(), 'Submit attendance')]"
+                )
                 if len(submit_attendance_btns) == 0:
                     continue
                 submit_attendance_btn = submit_attendance_btns[0]
-                print("Marking " + user['email'] +
-                      "'s attendance for " + course_name)
+                print("Marking " + user["name"] + "'s attendance for " + course_name)
                 att_link = submit_attendance_btn.get_attribute("href")
                 driver.get(att_link)
-                status_btn = driver.find_elements_by_xpath(
-                    "//input[@type='radio']")[0]
+                status_btn = driver.find_elements_by_xpath("//input[@type='radio']")[0]
                 status_btn.click()
                 driver.find_element_by_id("id_submitbutton").click()
                 print("Done")
 
             driver.get("http://moodle.mec.ac.in/login/logout.php")
             continue_btns = driver.find_elements_by_xpath(
-                "//*[contains(text(), 'Continue')]")
+                "//*[contains(text(), 'Continue')]"
+            )
             if len(continue_btns) == 0:
                 continue
             continue_btn = continue_btns[0]
             continue_btn.click()
-        time.sleep(20 * 60)
+        time.sleep(10 * 60)
 
 
 def create_arg_parser():
     # Creates and returns the ArgumentParser object
 
-    parser = argparse.ArgumentParser(
-        description='Automate your moodle attendance.')
-    parser.add_argument('--usersFile',
-                        help='Path to the JSON file that contains the credentials & courses of the user accounts for which automation is to be done.')
+    parser = argparse.ArgumentParser(description="Automate your moodle attendance.")
+    parser.add_argument("--config", help="Path to the JSON config file")
     return parser
 
 
 if __name__ == "__main__":
     arg_parser = create_arg_parser()
     parsed_args = arg_parser.parse_args(sys.argv[1:])
-    if not os.path.exists(parsed_args.usersFile):
-        print("Credentials file not found")
+    if not os.path.exists(parsed_args.config):
+        print("Config file not found")
         sys.exit(1)
-    users = parse_users(parsed_args.usersFile)
-    start_automation(users)
+
+    config = parse_config(parsed_args.config)
+    users = config["users"]
+    driver = config["driver"]
+    start_automation(users, driver)
--- individual.py
+++ individual.py
@@ -1,48 +1,62 @@
+# Import webdriver module from selenium
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+
+# Import time module for waiting 20 mins
 import time
-import os
-import sys
 
-moodle_pass = "test"
-moodle_mail = "test"
+# Create new driver object (change the file name based on your OS)
+driver = webdriver.Chrome("./chromedriver.exe")
 
-driver = webdriver.Chrome("C:/Users/aswin/auto-attendance/chromedriver.exe")
+# Visit the moodle login page and fill in login details
 driver.get("http://moodle.mec.ac.in")
+
+# Define Moodle email and password
+moodle_mail = "<your moodle mail>"
+moodle_pass = "<your moodle password>"
+
+# Find the field element and send the input keys to the element
 username_ip = driver.find_element_by_name("username")
-username_ip.clear()
 username_ip.send_keys(moodle_mail)
 pass_ip = driver.find_element_by_name("password")
-pass_ip.clear()
 pass_ip.send_keys(moodle_pass)
-pass_ip.send_keys(Keys.RETURN)
 
-if not driver.current_url == "http://moodle.mec.ac.in/my/":
-    print("Failed to log in")
-    sys.exit(1)
+# Find the login button using XPATH and click it
+login_btn = driver.find_element_by_xpath('//*[@id="login"]/div[4]/input')
+login_btn.click()
 
-courses = ["http://moodle.mec.ac.in/mod/attendance/view.php?id=685",
-           "http://moodle.mec.ac.in/mod/attendance/view.php?id=353",
-           "http://moodle.mec.ac.in/mod/attendance/view.php?id=557",
-           "http://moodle.mec.ac.in/mod/attendance/view.php?id=425",
-           "http://moodle.mec.ac.in/mod/attendance/view.php?id=1970",
-           "http://moodle.mec.ac.in/mod/attendance/view.php?id=683",
-           "http://moodle.mec.ac.in/mod/attendance/view.php?id=938"]
+# Define the list of attendance page URLs
+attendance_page_urls = [
+    "http://moodle.mec.ac.in/mod/attendance/view.php?id=9022",
+    "http://moodle.mec.ac.in/mod/attendance/view.php?id=9002",
+    "http://moodle.mec.ac.in/mod/attendance/view.php?id=8961",
+]
 
+# Start infinite loop
 while 1:
-    for course in courses:
-        print(course)
-        driver.get(course)
+    # For every URL visit the page
+    for url in attendance_page_urls:
+        driver.get(url)
+
+        # Get the list of all "Submit attendance" buttons on the page using XPATH
         submit_attendance_btns = driver.find_elements_by_xpath(
-            "//*[contains(text(), 'Submit attendance')]")
+            "//*[contains(text(), 'Submit attendance')]"
+        )
+
+        # If the list is empty, do nothing and continue
         if len(submit_attendance_btns) == 0:
             continue
+
+        # Else visit the link (HTML "href" attribute) specified by the first button
         submit_attendance_btn = submit_attendance_btns[0]
-        print("Marking...")
         att_link = submit_attendance_btn.get_attribute("href")
         driver.get(att_link)
+
+        # Find the fist attendance status radio button, ie, the one corresponding to "Present"
         status_btn = driver.find_elements_by_xpath("//input[@type='radio']")[0]
+
+        # Click the status button and then the submit button
         status_btn.click()
         driver.find_element_by_id("id_submitbutton").click()
-        print("Done")
-    time.sleep(10 * 60)
+
+    # Sleep for 20 mins before trying again
+    time.sleep(1 * 60)
